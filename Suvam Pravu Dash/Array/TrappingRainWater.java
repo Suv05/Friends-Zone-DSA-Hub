9c43212450709b858816c3e3ab6a70b2cd858c5c
suvam <suvampravu@gmail.com>
2024-10-26 01:44:55 +0530

    commit changes

--- Suvam Pravu Dash/Array/TrappingRainWater.java
+++ Suvam Pravu Dash/Array/TrappingRainWater.java
@@ -1,5 +1,31 @@
 public class TrappingRainWater {
-    public static void main(String[] args) {
 
+    public static void containerWithMostWater(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+
+            if (area > maxArea) {
+                maxArea = area;
+            }
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("Most area is " + maxArea);
+    }
+
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        containerWithMostWater(height);
     }
 }
