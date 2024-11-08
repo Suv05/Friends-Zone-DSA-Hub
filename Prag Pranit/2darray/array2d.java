09a8343ca7e1ad4a63d12c18ca5db05e6c579ca5
prag6328 <pranitprag@gmail.com>
2024-11-08 09:34:03 +0530

    commit changes

--- Prag Pranit/2darray/array2d.java
+++ Prag Pranit/2darray/array2d.java
@@ -1,7 +1,12 @@
 public class array2d {
     public static void main(String[] args) {
         //datatype [][] variable = new datatype [row size][colummnn size];
-        int  [][] arr = new int[3][];
-
+        //int  [][] arr = new int[3][];
+         /* 1 2 3
+            4 5 6
+            7 8 9
+          this is a 2d array
+         */
+        
     }
 }
