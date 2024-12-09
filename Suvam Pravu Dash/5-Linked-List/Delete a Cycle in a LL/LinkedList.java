1e7d04e531a6d2737780eb7a7538bfdc45e43671
suvam <suvampravu@gmail.com>
2024-12-09 23:45:45 +0530

    commit changes

--- Suvam Pravu Dash/5-Linked-List/Delete a Cycle in a LL/LinkedList.java
+++ Suvam Pravu Dash/5-Linked-List/Delete a Cycle in a LL/LinkedList.java
@@ -12,8 +12,44 @@ public class LinkedList {
     public Node head;
     public Node tail;
 
+    //addFirst()
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //print
+    public void print() {
+        Node temp = head;
+
+        if (temp == null) {
+            System.out.println("Linked list is null");
+        }
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println(" ");
+    }
+
 
     public static void main(String[] args) {
+
+        LinkedList ll= new LinkedList();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.print();
 
     }
 }
